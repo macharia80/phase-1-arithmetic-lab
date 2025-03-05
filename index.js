bb245905b87c895c67e0a180e94d0210cf82f48f
macharia <mrmachariaorsam@gmail.com>
2025-03-05 16:20:24 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+
+// Multiply two numbers to get 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Calculate a remainder that is equal to 4
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Return 20 as the highest number in the set
+const max = Math.max(1, 2, 3, 4, 20);
+
+console.log(multiply, random, mod, max);
